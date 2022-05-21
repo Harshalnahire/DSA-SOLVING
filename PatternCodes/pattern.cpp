e577ea9ee221a408578523339d8d71d6080765c7
Harshalnahire <nahireharsh@gmail.com>
2022-05-21 21:05:10 +0530

    Patterns

---
+++ PatternCodes/pattern.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    // int i =1;
+    // while (i<=3)
+    // {
+    //     // int j =1;
+        // while (j<=3)
+        // {
+        //     cout<<"*";
+        //      j=j+1;
+
+        // }
+        // cout<<endl;
+
+        // i=i+1;
+    // }
+
+    // int n;
+    // cin>>n;
+    // int i =1;
+    // while (i<=n)
+    // {
+    //    int j =1;
+    //    while (j<=n)
+    //    {
+    //        cout<<i;
+    //        j=j+1;
+    //    }
+    //     cout<<endl;
+    //     i =i +1;
+    // }
+    
+    
+// int n;
+// cin>>n;
+
+// int i= 1;
+// while (i<=n)
+// {
+//     int j=1;
+//     while (j<=n)
+//     {
+//         cout<<j;
+//         j=j+1;
+                 
+//     }
+//     cout<<endl;
+//     i=i+1;
+// }
+
+// int n;
+// cin>>n;
+
+// int i=1;
+// int count=1;
+// while (i<=n)
+// {
+//     int j=1;
+//     while (j<=n)
+//     {
+//         cout<<count<< " ";
+//         count=count+1;
+//         j=j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+
+
+// int n;
+// cin>>n;
+
+// int row=1;
+// while (row<=n)
+// {
+//     int col =1;
+//     while (col<=row)
+//     {
+//         cout<<"*";
+//         col=col+1;
+
+//     }
+//     cout<<endl;
+//     row=row+1;
+// int n;
+// cin>>n;
+
+// int row=1;
+// while (row<=n)
+// {
+//     int col =1;
+//     while (col<=row)
+//     {
+//         cout<<row;;
+//         col=col+1;
+
+//     }
+//     cout<<endl;
+//     row=row+1;
+    
+// }
+
+// using counting method
+// int n;
+// cin>>n;
+
+// int row=1;
+// int count=row;
+// while (row<=n)
+// {
+//     int col =1;
+//     while (col<=row)
+//     {
+//         cout<<count;
+//         count=count+1;
+//         col=col+1;
+
+//     }
+//     cout<<endl;
+//     row=row+1;
+int n;
+cin>>n;
+
+int row=1;
+
+while (row<=n)
+{
+    int col =1;
+    int value=row;
+    while (col<=row)
+    {
+        cout<<value;
+        value= value+1;
+        col=col+1;
+
+    }
+    cout<<endl;
+    row=row+1;
+}
+
+
+}
